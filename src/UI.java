1692a5dcf04a30c94118aa279c1786a0853404c8
Michal Pokorný <93346591+MichalPKN@users.noreply.github.com>
2023-01-07 00:40:28 +0100

    Add files via upload

--- src/UI.java
+++ src/UI.java
@@ -9,19 +9,23 @@ import java.util.Scanner;
 public class UI {
 
     /**
-     * hlavní main metoda
+     * hlavní metoda
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean konec = false;
         while(!konec){
+            
+            //input
             System.out.println("vyber ulohu, kterou chces spustit (vanocni/semestralni/konec):");
             String uloha = sc.next().toLowerCase();
+            
+            //vyber ulohy
             if (uloha.equals("vanocni")){
                 Christmas.present();
                 System.out.println("");
-            }else if(uloha.equals("semestralni")){ // changeeee na semestralni
+            }else if(uloha.equals("semestralni")){
                 TrianglePoints.bodyVuciTrojuhleniku();
             }else if(uloha.equals("konec")){
                 konec = true;
